95f053162b9f3d341e380218bd0e029ec6b75dd9
Brandon Woodson <b@brandonwoodson.com>
2016-12-21 01:36:56 -0500

    First commit
    My PowerShell profile setup.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,29 @@
+# Grabs the directory that the script is running from
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+# Load longer form functions
+# https://github.com/scottmuc/poshfiles/blob/master/Microsoft.PowerShell_profile.ps1
+Resolve-Path $scriptDir\Functions\*.ps1 | % { . $_.ProviderPath }
+
+# Shortcuts I use on a regular basis
+function gh() { Set-Location $env:USERPROFILE }
+function gd() { Set-Location ($env:USERPROFILE + "\Documents\Dev") }
+function ggh() { Set-Location ($env:USERPROFILE + "\Documents\GitHub") }
+function x() { exit }
+function reboot() { shutdown -r -t 1 }
+
+# Launch Microsoft Visual Studio Code
+New-Alias -name c -value code
+
+# Colors
+$console = $host.UI.RawUI
+$console.ForegroundColor = "White"
+$console.BackgroundColor = "Black"
+
+# Add posh-git from GitHub Shell to PowerShell
+# http://stackoverflow.com/questions/12504649/how-to-use-posh-git-that-comes-with-github-for-windows-from-custom-shell
+. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
+. $env:github_posh_git\profile.example.ps1
+
+# Clear to set colors
+Clear-Host
---
+++ README.md
@@ -0,0 +1,18 @@
+# PowerShell Profile
+
+### Description
+
+Here's my PowerShell profile. Some highlights:
+
+* Current directory only path name
+* Black and white color scheme
+* Custom Aliases
+* Search and load custom functions
+
+### Installation
+
+```
+git clone git@github.com:BWoodson/PS-Profile %USERPROFILE%\Documents\WindowsPowershell
+```
+
+Replace|Modify the **profile.example.ps1** file from posh-git to change the prompt.
